10ea5d9f167def57ef5ee32c20388655e360e60e
Екатерина <cmygla@mail.ru>
2019-03-24 16:38:35 +0400

    Added support classes with parameters and properties

---
+++ addressbook-web-tests/addressbook-web-tests/GroupData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class GroupData
+    {
+        private string name;
+        private string header = "";
+        private string footer = "";
+
+        public GroupData(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public string Header
+        {
+            get
+            {
+                return header;
+            }
+
+            set
+            {
+                this.header = value;
+            }
+        }
+        public string Footer
+        {
+            get
+            {
+                return footer;
+            }
+
+            set
+            {
+                this.footer = value;
+            }
+        }
+    }
+}
+
