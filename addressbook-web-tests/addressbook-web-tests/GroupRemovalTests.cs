d135244d8f91f4ad99b046cbcb268547a22fe69b
Екатерина <cmygla@mail.ru>
2019-03-31 21:22:50 +0400

    create TestBase class with additional methods

---
+++ addressbook-web-tests/addressbook-web-tests/GroupRemovalTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace addressbook_web_tests
+{
+    [TestFixture]
+    public class GroupRemovalTest : TestBase
+    {
+
+        [Test]
+        public void TheUntitledTestCaseTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            GroupSelection();
+            GroupDeletion();
+            ReturnToGroupsPage();
+            Logout();
+        }
+    }
+}
